e1aee76ff799eaa943b0b05dcf55f46121490898
obiim <obiim@xubuntu.myguest.virtualbox.org>
2025-03-28 18:29:46 +0100

    chore:initial commit

---
+++ foundrySigner/src/Counter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+
+contract TokenMint  is ERC20{
+    uint256 public number;
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+}
